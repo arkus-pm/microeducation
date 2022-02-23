86c7bf32a66b7be2543b65b3d249f04e1aa35b7a
Maxim <intmaxfactor@gmail.com>
2022-02-23 17:10:28 +0200

    updated layout,added uischeme,default values

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { JsonForms } from '@jsonforms/react';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 import logo from './logo.svg';
 import './App.css';
 import schema from './schema.json';
 import uischema from './uischema.json';
+import { UISchemaElement } from '@jsonforms/core';
 import {
   materialCells,
   materialRenderers,
@@ -15,6 +17,19 @@ import RatingControl from './RatingControl';
 import ratingControlTester from './ratingControlTester';
 import { makeStyles } from '@mui/styles';
 import { createAjv } from '@jsonforms/core';
+import { Generate } from '@jsonforms/core';
+
+// const uischema = Generate.uiSchema(schema);
+let arr = [
+Generate.uiSchema(schema),
+Generate.uiSchema(schema.properties.courses.items),
+Generate.uiSchema(schema.properties.courses.items.properties.episodes.items),
+Generate.uiSchema(schema.properties.courses.items.properties.episodes.items.properties.stories.items),
+Generate.uiSchema(schema.properties.courses.items.properties.card),
+Generate.uiSchema(schema.properties.courses.items.properties.preview)
+];
+arr.forEach(e=>console.log(JSON.stringify(e)));
+
 
 const handleDefaultsAjv = createAjv({useDefaults: true});
 
@@ -22,17 +37,21 @@ const useStyles = makeStyles({
   container: {
     padding: '1em',
     width: '100%',
+    height: 'auto'
   },
   title: {
     textAlign: 'center',
     padding: '0.25em',
   },
   dataContent: {
+    overflow: 'auto',
     display: 'flex',
-    justifyContent: 'center',
+    justifyContent: 'left',
     borderRadius: '0.25em',
-    backgroundColor: '#cecece',
+    backgroundColor: '#fefefe',
     marginBottom: '1rem',
+    maxHeight: '300px',
+    width: '100%'
   },
   resetButton: {
     margin: 'auto !important',
@@ -44,7 +63,7 @@ const useStyles = makeStyles({
   },
 });
 
-const initialData = {"version":1.23,"contentURL":"","courses":[{"version":1.23123,"id":"bddab411-0bca-44f8-92c1-9204555d8045","flowTopic":"QA test","card":{"title":"Forge of the heart","imageURL":"http://mw1.google.com/mw-planetary/sky/skytiles_v1/50_49_6.jpg","description":"Some description..."},"preview":{"title":"Forge of the heart","imageURL":"http://mw1.google.com/mw-planetary/sky/skytiles_v1/50_49_6.jpg","overview":"Some overview..."},"episodes":[{"id":"5b171580-813e-415b-8b3c-b69c546ba54f","position":0,"title":"Focus on something else","description":"Learn technics to control...","stories":[{"id":"648a1650-22d6-4d75-8167-8eb90c0c79a2","version":1.23,"position":1,"transition":{"type":"fade_in"},"content":{"type":"textIllustration","illustrationURL":"http://mw1.google.com/mw-planetary/sky/skytiles_v1/50_49_6.jpg","text":"<b>Greetings<\\b>\n\nLet\\s start our journey with a few kind words about yourself\n\nYou are brave to be here!"}}]},{"id":"f33cc592-f16c-4701-8d95-44e71e85468b","position":1,"title":"Episode 2","description":"Episode 2 description","stories":[{"id":"b48b01d7-05f8-469b-9034-a1af04a28fca","version":1.23,"position":1,"transition":{"type":"fade_in"},"content":{"type":"textIllustration","illustrationURL":"http://mw1.google.com/mw-planetary/sky/skytiles_v1/50_49_6.jpg","text":"<b>Greetings<\\b>\n\nLet\\s start our journey with a few kind words about yourself\n\nYou are brave to be here!"}}]}]}]};
+const initialData = {};//{  "type": "object",  "properties": {    "name": {      "type": "string"    },    "vegetarian": {      "type": "boolean"    },    "birthDate": {      "type": "string"    },    "personalData": {      "type": "object",      "properties": {        "age": {          "type": "integer"        }      },      "additionalProperties": true,      "required": [        "age"      ]    },    "postalCode": {      "type": "string"    }  },  "additionalProperties": true,  "required": [    "name",    "vegetarian",    "birthDate",    "personalData",    "postalCode"  ]}//{"version":1.23,"contentURL":"","courses":[{"version":1.23123,"id":"bddab411-0bca-44f8-92c1-9204555d8045","flowTopic":"QA test","card":{"title":"Forge of the heart","imageURL":"http://mw1.google.com/mw-planetary/sky/skytiles_v1/50_49_6.jpg","description":"Some description..."},"preview":{"title":"Forge of the heart","imageURL":"http://mw1.google.com/mw-planetary/sky/skytiles_v1/50_49_6.jpg","overview":"Some overview..."},"episodes":[{"id":"5b171580-813e-415b-8b3c-b69c546ba54f","position":0,"title":"Focus on something else","description":"Learn technics to control...","stories":[{"id":"648a1650-22d6-4d75-8167-8eb90c0c79a2","version":1.23,"position":1,"transition":{"type":"fade_in"},"content":{"type":"textIllustration","illustrationURL":"http://mw1.google.com/mw-planetary/sky/skytiles_v1/50_49_6.jpg","text":"<b>Greetings<\\b>\n\nLet\\s start our journey with a few kind words about yourself\n\nYou are brave to be here!"}}]},{"id":"f33cc592-f16c-4701-8d95-44e71e85468b","position":1,"title":"Episode 2","description":"Episode 2 description","stories":[{"id":"b48b01d7-05f8-469b-9034-a1af04a28fca","version":1.23,"position":1,"transition":{"type":"fade_in"},"content":{"type":"textIllustration","illustrationURL":"http://mw1.google.com/mw-planetary/sky/skytiles_v1/50_49_6.jpg","text":"<b>Greetings<\\b>\n\nLet\\s start our journey with a few kind words about yourself\n\nYou are brave to be here!"}}]}]}]};
 
 const renderers = [
   ...materialRenderers,
@@ -61,41 +80,61 @@ const App = () => {
     setData({});
   };
 
+const [displayBound, setDisplayBound] = useState<boolean>(true);
+const toggleJSONView = () => {setDisplayBound(!displayBound)};
+
   return (
     <Fragment>
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to JSON Forms with React</h1>
-          <p className='App-intro'>More Forms. Less Code.</p>
-        </header>
-      </div>
 
       <Grid
         container
-        justifyContent={'center'}
-        spacing={1}
+        direction="column"
+        justifyContent="center"
+        spacing={2}
         className={classes.container}
       >
         <Grid item sm={6}>
-          <Typography variant={'h4'} className={classes.title}>
-            Bound data
-          </Typography>
-          <div className={classes.dataContent}>
-            <pre id='boundData'>{stringifiedData}</pre>
-          </div>
+          {displayBound && <Typography variant={'h4'} className={classes.title}>
+            JSON
+          </Typography>}
+           {displayBound &&  <TextField
+            className={classes.dataContent}
+            id='boundData'
+            multiline
+            variant="outlined" 
+            value={stringifiedData}
+            onChange={(event)=>{ setData(JSON.parse(event.target.value));}}
+            >
+            </TextField> }
+          <Grid
+          container
+          justifyContent="center"
+          direction="row"
+          spacing={2}
+          >
+          <Grid item>
           <Button
-            className={classes.resetButton}
             onClick={clearData}
             color='primary'
             variant='contained'
           >
             Clear data
           </Button>
+          </Grid>
+          <Grid item>
+          <Button
+            onClick={toggleJSONView}
+            color='secondary'
+            variant='contained'
+          >
+            {!displayBound?'Show':'Hide'} JSON
+          </Button>
+          </Grid>
+          </Grid>
         </Grid>
         <Grid item sm={6}>
           <Typography variant={'h4'} className={classes.title}>
-            Rendered form
+            MicroEd Form
           </Typography>
           <div className={classes.demoform}>
             <JsonForms
@@ -104,7 +143,7 @@ const App = () => {
               data={data}
               renderers={renderers}
               cells={materialCells}
-              onChange={({ errors, data }) => setData(data)}
+              onChange={({ errors, data }) => {setData(data);}}
               ajv={handleDefaultsAjv}
             />
           </div>
